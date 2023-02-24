0bb727f5699188430d969c9439e77f8df9cdac3c
Timur <galy.tim@gmail.com>
2023-02-24 16:13:35 +0300

    Add devise && ref between user post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,10 @@ class Post < ApplicationRecord
 
     include ImageUploader::Attachment(:image)
     include Visible
+    belongs_to :user
     has_many :comments, dependent: :destroy
 
-
     validates :image, presence: true
-
     validates :body, presence: true, length: { minimum: 5 }
 
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,24 +1,15 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded any time
-  # it changes. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports.
   config.consider_all_requests_local = true
 
-  # Enable server timing
   config.server_timing = true
 
-  # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
@@ -58,13 +49,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
-
-  # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
